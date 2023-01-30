9f571571583e4833be406e6230a9eea8578955e4
Hellinduction <joseph.connor.west@icloud.com>
2023-01-30 20:12:29 +0000

    Revert "Various improvements and added new feature"
    This reverts commit dd543aae758c36ee5cc2ce62d15c3efcccd97a02.

--- src/main/java/me/headzz/discordbanannouncer/utility/GenericMessages.java
+++
@@ -1,9 +0,0 @@
-package me.headzz.discordbanannouncer.utility;
-
-public class GenericMessages {
-    public static final String USERNAME = "Username";
-    public static final String PUNISHED_BY = "Punished by";
-    public static final String REASON = "Reason";
-    public static final String DURATION = "Duration";
-    public static final String REMOVED_BY = "Removed by";
-}
